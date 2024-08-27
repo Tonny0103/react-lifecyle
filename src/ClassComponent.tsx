2069efce76fc297d4d83ffe7e676286b4fbb4d9f
Tonny <anthonny14gabriell@gmail.com>
2024-08-27 09:29:24 -0400

    creating a class component and a functional component

---
+++ src/ClassComponent.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+type Props = {};
+
+type State = {};
+
+class ClassComponent extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return <div>ClassComponent</div>;
+  }
+}
+
+export default ClassComponent;
---
+++ src/FunComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const FunComponent = (props: Props) => {
+  return <div>FunComponent</div>;
+};
+
+export default FunComponent;
